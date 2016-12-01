e0ace33edcc75d57cf14888331a01511c4f09575
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2016-12-01 22:58:00 +0100

    Implementados los tests de aceptación con Protractor

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { Ng2TallerPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('ng2-taller App', function() {
   let page: Ng2TallerPage;
@@ -9,6 +10,25 @@ describe('ng2-taller App', function() {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Search User');
   });
+
+  it('should display login', () => {
+    page.navigateTo();
+    const USERNAME = 'raguilera82';
+    page.setUsername(USERNAME);
+    page.getButtonSearch().click();
+    expect(page.getLogin().getText()).toContain(USERNAME);
+    browser.driver.sleep(5000);
+  });
+
+  it('should display error', () => {
+    page.navigateTo();
+    const USERNAME = 'raguilera82error';
+    page.setUsername(USERNAME);
+    page.getButtonSearch().click();
+    expect(page.getError().isElementPresent).toBeTruthy();
+    browser.driver.sleep(5000);
+  });
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -8,4 +8,20 @@ export class Ng2TallerPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  setUsername(username: string) {
+    element(by.css('#username')).sendKeys(username);
+  }
+
+  getButtonSearch() {
+    return element(by.css('#search'));
+  }
+
+  getLogin() {
+    return element(by.css('#login'));
+  }
+
+  getError() {
+    return element(by.css('#error'));
+  }
 }
